d6e586d1aa0516f16e7c7c61f28c7fc5553686bd
Chriss Philip Saji <chrissattasseril16@gmail.com>
2025-06-30 22:53:07 +0530

    Remove Reddit authentication and fix TypeScript errors

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,8 +3,6 @@
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_ANON_KEY: string
-  readonly VITE_REDDIT_CLIENT_ID: string
-  readonly VITE_REDDIT_CLIENT_SECRET: string
 }
 
 interface ImportMeta {
